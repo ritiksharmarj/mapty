532de707a28624eef0c7dbbf1bf7f0419e953e6c
ritiksharmarj <theonlyritik@gmail.com>
2023-02-13 16:04:57 +0530

    Rendering Workouts

--- script.js
+++ script.js
@@ -21,6 +21,10 @@ class Workout {
    _setDescription() {
       // prettier-ignore
       const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+      this.description = `${this.type[0].toUpperCase()}${this.type.slice(
+         1
+      )} on ${months[this.date.getMonth()]} ${this.date.getDate()}`;
    }
 }
 
@@ -30,8 +34,8 @@ class Running extends Workout {
    constructor(coords, distance, duration, cadence) {
       super(coords, distance, duration);
       this.cadence = cadence;
-
       this.calcPace();
+      this._setDescription();
    }
 
    calcPace() {
@@ -47,8 +51,8 @@ class Cycling extends Workout {
    constructor(coords, distance, duration, elevationGain) {
       super(coords, distance, duration);
       this.elevationGain = elevationGain;
-
       this.calcSpeed();
+      this._setDescription();
    }
 
    calcSpeed() {
@@ -118,6 +122,19 @@ class App {
       inputDistance.focus();
    }
 
+   _hideForm() {
+      // Empty inputs
+      inputDistance.value =
+         inputDuration.value =
+         inputCadence.value =
+         inputElevation.value =
+            '';
+
+      form.style.display = 'none';
+      form.classList.add('hidden');
+      setTimeout(() => (form.style.display = 'grid'), 1000);
+   }
+
    // Form input type toggle between Running and Cycling
    _toggleElevationField() {
       inputElevation
@@ -179,13 +196,10 @@ class App {
       this._renderWorkout(workout);
 
       // Hide form + clear input fields
-      inputDistance.value =
-         inputDuration.value =
-         inputCadence.value =
-         inputElevation.value =
-            '';
+      this._hideForm();
    }
 
+   // Render workout on the map as marker
    _renderWorkoutMarker(workout) {
       L.marker(workout.coords)
          .addTo(this.#map)
@@ -198,14 +212,19 @@ class App {
                className: `${workout.type}-popup`,
             })
          )
-         .setPopupContent(workout.type)
+         .setPopupContent(
+            `${workout.type === 'running' ? '🏃‍♂️' : '🚴‍♀️'} ${workout.description}`
+         )
          .openPopup();
+
+      console.log(workout);
    }
 
+   // Render workout in the list
    _renderWorkout(workout) {
-      const html = `
+      let html = `
          <li class="workout workout--${workout.type}" data-id="${workout.id}">
-            <h2 class="workout__title">Running on April 14</h2>
+            <h2 class="workout__title">${workout.description}</h2>
             <div class="workout__details">
                <span class="workout__icon">${
                   workout.type === 'running' ? '🏃‍♂️' : '🚴‍♀️'
@@ -219,6 +238,40 @@ class App {
                <span class="workout__unit">min</span>
             </div>
       `;
+
+      if (workout.type === 'running') {
+         html += `
+            <div class="workout__details">
+               <span class="workout__icon">⚡️</span>
+               <span class="workout__value">${workout.pace.toFixed(1)}</span>
+               <span class="workout__unit">min/km</span>
+            </div>
+            <div class="workout__details">
+               <span class="workout__icon">🦶🏼</span>
+               <span class="workout__value">${workout.cadence}</span>
+               <span class="workout__unit">spm</span>
+            </div>
+         </li>
+         `;
+      }
+
+      if (workout.type === 'cycling') {
+         html += `
+            <div class="workout__details">
+               <span class="workout__icon">⚡️</span>
+               <span class="workout__value">${workout.speed.toFixed(1)}</span>
+               <span class="workout__unit">km/h</span>
+            </div>
+            <div class="workout__details">
+               <span class="workout__icon">⛰</span>
+               <span class="workout__value">${workout.elevationGain}</span>
+               <span class="workout__unit">m</span>
+            </div>
+         </li>
+         `;
+      }
+
+      form.insertAdjacentHTML('afterend', html);
    }
 }
 
